3acde6805435127f26ffb7fe6fa3c4f9bfb7d0d2
kurtn2005 <kurtn@126.com>
2019-04-01 17:03:43 +0800

    Update wechat_livec.php
    Add auto refresh comments function include jquery.js and query.js, remove comments code to another file 'bbs.php'.

--- wechat_livec.php
+++ wechat_livec.php
@@ -10,6 +10,8 @@
     <script type="text/javascript" src="statistics.js" charset="UTF-8"></script>
     <script type="text/javascript" src="04.js" charset="UTF-8"></script>
     <script type="text/javascript" src="checkInput.js" charset="UTF-8"></script>
+    <script type="text/javascript" src="js/jquery-1.11.3.min.js" charset="UTF-8"></script>
+    <script type="text/javascript" src="js/query.js" charset="UTF-8"></script>
     <title>某某现场直播</title>
 </head>
 <body>
@@ -47,27 +49,6 @@
 </div>
 <div id="d1">
     <div class="ylcon">
-        <!--<p class="tit">所有留言</p>-->
-<?php 
-    $mysqli = new mysqli("localhost", "root", "db", "guestbook"); 
-    if(!$mysqli)  { 
-        echo"database error"; 
-    }else{
-        $result=mysqli_query($mysqli, "select * from gbook order by sn desc limit 10");
-        while($row = mysqli_fetch_assoc($result)){
-?>
-        <div id="messDivId">
-            <div class="story">
-                <!--<div class="opbtn"></div>-->
-                <p class="story_t"><?php echo $row['user'];?></p>
-                <p class="story_time"><?php echo $row['time'];?></p>
-                <p class="story_m"><?php echo $row['message'];?></p>
-            </div>
-        </div>
-<?php }
-   }
-   $mysqli->close();
-?>
         <form method="POST" action="insert.php" class="bootstrap-frm">
             <div>
                 <label>
@@ -79,8 +60,6 @@
                     <textarea id="message" rows="2" name="S1" cols="38" onFocus="this.value=''">不超过38个汉字</textarea>
                 </label>
                 <label class="submit-label">
-                    <!--<span></span>-->
-                    <!--<span></span>-->
                     <input type="reset" class="button" value="重置" name="B2">
                     <input type="submit" class="button" value="提交" name="B1" onclick="return checkInput()"/>
                 </label>
